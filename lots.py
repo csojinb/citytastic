20f856ec4109b30dda1c1ed5553814f2897f4c56
Chris Thatcher <cthatcher@fool.com>
2014-11-10 01:33:15 -0500

    Making lots_per_road_point a passed-in argument so that it can be changed in main.py easily (the only file Blender cares to update)

--- lots.py
+++ lots.py
@@ -2,10 +2,7 @@ from collections import namedtuple
 import math
 import random
 
-import mathutils
-
 from models import BuildingLot
-from rendering import draw_line
 from roads import get_perpendicular_directions
 
 ROAD_CLEARANCES = {
@@ -15,24 +12,22 @@ ROAD_CLEARANCES = {
     3: 0.02,
 }
 
-LOTS_PER_ROAD_POINT = 0.8
-
 
 LotEdge = namedtuple('LotEdge', ['corner', 'direction'])
 
 
-def select_lots(roads):
+def select_lots(roads, lots_per_road_point):
     building_lots = []
     for road in roads:
-        building_lots.extend(_select_lots_on_road(road))
+        building_lots.extend(_select_lots_on_road(road, lots_per_road_point))
 
     return building_lots
 
 
-def _select_lots_on_road(road):
+def _select_lots_on_road(road, lots_per_road_point):
     building_lots = []
     for side in [0, 1]:
-        lot_edges = _select_lot_edges_on_road(road, side)
+        lot_edges = _select_lot_edges_on_road(road, side, lots_per_road_point)
         building_lots.extend(_calculate_lots_from_edges(lot_edges))
 
     return building_lots
@@ -55,18 +50,18 @@ def _calculate_lots_from_edges(lot_edges):
     return building_lots
 
 
-def _select_lot_edges_on_road(road, side):
-    number_of_edges = math.floor(LOTS_PER_ROAD_POINT * len(road.points))
+def _select_lot_edges_on_road(road, side, lots_per_road_point):
+    number_of_edges = math.floor(lots_per_road_point * len(road.points))
     clearance = ROAD_CLEARANCES[road.road_size]
 
     addresses = random.sample(list(enumerate(road.points)), number_of_edges)
     addresses.sort(key=lambda x: x[0])
-    lot_edges = _select_lot_edges_on_road_side(addresses, clearance, side)
+    lot_edges = _select_lot_edges_on_road_side(road, addresses, clearance, side)
 
     return lot_edges
 
 
-def _select_lot_edges_on_road_side(addresses, clearance, side):
+def _select_lot_edges_on_road_side(road, addresses, clearance, side):
     lot_edges = []
     for index, address in addresses:
         try:
@@ -78,4 +73,3 @@ def _select_lot_edges_on_road_side(addresses, clearance, side):
         lot_edges.append(LotEdge(corner=corner, direction=perpendicular))
 
     return lot_edges
-
