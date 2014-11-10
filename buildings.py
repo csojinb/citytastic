0a746b5e8bae40ccd559b4bfa9a28e622852362d
Chris Thatcher <cthatcher@fool.com>
2014-11-10 01:34:07 -0500

    Utilizing lots to place buildings as opposed to the naive solution

--- buildings.py
+++ buildings.py
@@ -8,7 +8,6 @@ def random_within_range(min_val, max_val):
 
 
 def _find_closest_heatmap_tile(point, heatmap):
-    print (point)
     closest_tile = (None, float("inf"))
     for row in heatmap:
         for tile in row:
@@ -18,21 +17,21 @@ def _find_closest_heatmap_tile(point, heatmap):
     return closest_tile[0]
 
 
-def generate_buildings(heatmap, building_positions):
+def generate_buildings(heatmap, lots):
     buildings = []
 
     min_placeable_height = 0.4
     max_height = 4
 
-    for position in building_positions:
-        closest_tile = _find_closest_heatmap_tile(position[0], heatmap)
+    for lot in lots:
+        closest_tile = _find_closest_heatmap_tile(lot.center, heatmap)
 
         building_heat_value = (closest_tile.value[1] ** 0.25) / 5
         if building_heat_value > min_placeable_height:
             building_height = random_within_range(building_heat_value * 0.5, building_heat_value * 1.5)
             if building_height > max_height:
                 building_height = random_within_range(max_height * 0.8, max_height)
-            new = Building(position=position[0], width=position[1], height=position[2], depth=building_height / 2)
+            new = Building(position=lot.center, width=lot.base_size, height=lot.base_size, depth=building_height / 2)
             buildings += [new]
 
     for building in buildings:
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from roads import spawn_roads_within_grid
 from heatmap import generate_heatmap
-# from buildings import generate_buildings
-from naive_implementation import get_buildings
+from buildings import generate_buildings
 from models import Grid
 import bpy
 import mathutils
@@ -10,12 +9,13 @@ import time
 from lots import select_lots
 
 HEATMAP_SIZE = 20
+LOTS_PER_ROAD_POINT = 0.5
 
 
 def cleanup():
     should_delete = False
     for key in bpy.data.objects.keys():
-        if "road" in key or "Building" in key:
+        if "road" in key or "Building" in key or "lot" in key:
             bpy.data.objects.get(key).select = True
             should_delete = True
 
@@ -26,6 +26,7 @@ def cleanup():
 # first, delete the objects generated and drawn from the last city generated
 cleanup()
 
+
 timer_start = time.time()
 grid = Grid(mathutils.Vector((0, 0, 0)), 5, 5)
 
@@ -33,21 +34,18 @@ spawned_roads = spawn_roads_within_grid(grid, 3)
 heatmap = generate_heatmap(grid, spawned_roads, HEATMAP_SIZE)
 
 
+lots = select_lots(spawned_roads, LOTS_PER_ROAD_POINT)
+
+
+buildings = generate_buildings(heatmap, lots)
+
+
 for road in spawned_roads:
     road.draw()
-
-buildings = get_buildings(heatmap)
-
+for lot in lots:
+    lot.draw()
 for b in buildings:
     b.draw()
 
-lots = select_lots(spawned_roads)
-
-for lot in lots:
-    lot.draw()
-
 
 print("timed the whole thing {0}".format(time.time() - timer_start))
-
-
-print("done")
